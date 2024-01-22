2a7f585ba5413e51fa7a2222e267e233a87db0fc
Utku Ege Tuluk <36725861+uetuluk@users.noreply.github.com>
2024-01-22 16:36:37 +0800

    fix: nyu

--- geo_functions.py
+++ geo_functions.py
@@ -110,7 +110,7 @@ global street_name_matching_mapping
 street_name_matching_mapping = {}
 
 while True:
-  data_folder_choice = "NYU"
+  data_folder_choice = "nyu"
   if data_folder_choice.startswith('n'):
     data_folder_path = '/content/drive/MyDrive/HIST SHU 239 NYC 2024 CoLab/CoLab Data'
     break
